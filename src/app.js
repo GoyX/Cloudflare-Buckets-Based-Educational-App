341aca098e1fc57891e2ad7cdba1adadc33e2d66
NetSurfing <www.pqowieuryt0@gmail.com>
2026-08-15 00:17:36 +0300

    Change health check endpoint response format

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/healthz", (req, res) => {
-  res.render("offline");
+  res.status(200).json({ success: "true" });
 });
 
 app.use(counter.count);
